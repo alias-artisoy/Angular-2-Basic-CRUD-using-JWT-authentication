89012a763bf322a2d21adede0af3a3ca8b0ba284
artfernandez <artisoyfernandez@gmail.com>
2017-11-02 10:12:45 +0800

    fixes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,17 +1,14 @@
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
-import { HomeComponent } from './home/home.component';
-import { MainComponent } from './main/main.component';
+import { SpeechesComponent } from './speeches/speeches.component';
+import { SpeechDetailsComponent } from './speech-details/speech-details.component';
 
 
 const APP_ROUTES: Routes = [
-    { path: 'signin', component:SigninComponent},
-    { path: 'signup', component:SignupComponent},
-    { path: 'home', component:HomeComponent},
-    { path: '', component:MainComponent}        
+    { path: 'speeches', component:SpeechesComponent},
+    { path: 'speechdetails', component:SpeechDetailsComponent},
+    { path: '', component:SpeechesComponent}        
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
